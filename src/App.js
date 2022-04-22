3038587375b0765f03de5fd609f998663e319b36
임빛나 <devbit4@gmail.com>
2022-04-23 02:04:31 +0900

    샘플 동영상 삽입

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import react from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import ReactPlayer from 'react-player';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Typography variant='h6'>React Video Player</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Container maxWidth='md'>
+        <ReactPlayer
+          url='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+          muted={true}
+          playing={true}
+        ></ReactPlayer>
+      </Container>
+    </>
+  );
 }
 
 export default App;
