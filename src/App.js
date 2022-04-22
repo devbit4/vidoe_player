31b6ab3660993ec32b4c5a6fcee35cc60efc248a
임빛나 <devbit4@gmail.com>
2022-04-23 02:28:46 +0900

    create player wrapper and control wrapper

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import ReactPlayer from 'react-player';
+import { styled } from '@mui/system';
+
+const PlayerWrapper = styled('div')({
+  width: '100%',
+  backgroundColor: 'red ',
+  position: 'relative',
+});
+const ControlsWrapper = styled('div')({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0,0,0,0.6)',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  zIndex: 1,
+});
 
 function App() {
   return (
@@ -15,11 +34,16 @@ function App() {
       </AppBar>
       <Toolbar />
       <Container maxWidth='md'>
-        <ReactPlayer
-          url='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-          muted={true}
-          playing={true}
-        ></ReactPlayer>
+        <PlayerWrapper>
+          <ReactPlayer
+            width={'100%'}
+            height={'100%'}
+            url='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+            muted={true}
+            playing={true}
+          />
+          <ControlsWrapper></ControlsWrapper>
+        </PlayerWrapper>
       </Container>
     </>
   );
